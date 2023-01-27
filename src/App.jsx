e0597bba190f73f415cd991170a93991b463fdb4
Daniel <mattosfacul@gmail.com>
2023-01-27 10:52:01 -0300

    Create global styles, and add baseic assets.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Experience from './components/Experience/Experience'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import Portfolio from './components/Portfolio/Portfolio'
+
 const app = () => {
   return (
-    <div>app</div>
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Portfolio />
+        <Contact />
+        <Footer />
+    </>
   )
 }
 
